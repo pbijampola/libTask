5339307926d676d63ada788f64127cc193542700
PATRICK BIJAMPOLA <patrickbijampola@gmail.com>
2023-01-25 09:06:38 +0300

    restructuring

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,26 +5,14 @@
             @foreach ($books as $book)
                 <div class="col-md-4 mb-3">
                     <div class="card" style="width: 18rem;">
-                        <img src="{{ asset('assets/uploads/books' . $book->image) }}" class="card-img-top" alt="image">
+                        <img src="{{asset('assets/uploads/books'.$book->image)}}" class="card-img-top" alt="image">
 
                         <div class="card-body">
                             <h3 class="card-title">{{ $book->tilte }}</h3>
                             <p class="card-text">{{ $book->short_decs }}</p>
                             <div class="card-footer d-flex justify-content-between">
 
-                                {{--  @if (!$book->likes()->where('user_id', Auth::id())->first())
-                                    <form method="post" action="{{ route('books.like', $book) }}">
-                                        @csrf
-                                        <button type="submit" class="btn btn-success"><i class="bi bi-heart"></i></button>
-                                    </form>
-                                @else
 
-                                 (!$book->likes()->where('user_id', Auth::id())->first())
-                                    <form method="post" action="{{ route('books.dislike', $book) }}">
-                                        @csrf
-                                        <button type="submit" class="btn btn-danger"><i class="bi bi-heart"></i></button>
-                                    </form>
-                                @endif  --}}
                                 @php
                                     $existingLike = $book
                                         ->likes()
@@ -55,25 +43,7 @@
                                 <a href="{{ route('book.show', $book->id) }}" class="btn btn-info">
                                     <i class="bi bi-chat"></i>
                                 </a>
-                                {{--  @php
-                                    $existingFavorite = $book
-                                        ->favorites()
-                                        ->where('user_id', Auth::id())
-                                        ->first();
-                                @endphp  --}}
-                                {{--  @if (!$existingFavorite)
-                                    <form method="post" action="{{ route('books.markAsFavorite', $book) }}">
-                                        @csrf
-                                        <button type="submit" class="btn btn-success">Mark as Favorite</button>
-                                    </form>
-                                @endif
 
-                                @if ($existingFavorite)
-                                    <form method="post" action="{{ route('books.unmarkAsFavorite', $book) }}">
-                                        @csrf
-                                        <button type="submit" class="btn btn-danger">Unmark as Favorite</button>
-                                    </form>
-                                @endif  --}}
                                 @if (!$book->users()->wherePivot('is_favorite', true)->where('user_id', Auth::id())->first())
                                     <form method="post" action="{{ route('books.markAsFavorite', $book) }}">
                                         @csrf
@@ -87,25 +57,6 @@
                                         <button type="submit" class="btn btn-danger"><i class="bi bi-heart"></i></button>
                                     </form>
                                 @endif
-
-
-
-
-
-
-
-                                {{--  @if (Auth::user()->favoriteBooks->contains($book))
-                                    <form method="post" action="{{ route('users.unmarkAsFavorite', $book) }}">
-                                        @csrf
-                                        <button type="submit" class="btn btn-danger">Unmark as Favorite</button>
-                                    </form>
-                                @else
-                                <form method="post" action="{{ route('users.markAsFavorite', ['user' => $user->id, 'book' => $book->id]) }}">
-
-                                        @csrf
-                                        <button type="submit" class="btn btn-success">Mark as Favorite</button>
-                                    </form>
-                                @endif  --}}
                             </div>
                         </div>
                     </div>
